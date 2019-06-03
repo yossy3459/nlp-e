3fb784e4d9bbb825fa910f3e3bc337c000e275f1
ma19013 <ma19013@shibaura-it.ac.jp>
2019-06-03 23:32:37 +0900

    sorted in contents of result

--- Summarizer.py
+++ Summarizer.py
@@ -12,12 +12,13 @@ def summarizer(document_list: str, high_rank_all: dict):
     """
     summarize = []
     for document, high_rank in zip(document_list, high_rank_all):
-        summarize_text = set()
+        summarize_text = list()
         document = re.split("[ 。・\?\!？！■]", document)
         for sentence in document:
             for hr in list(high_rank.keys()):
                 if hr in sentence:
-                    summarize_text.add(sentence)
+                    summarize_text.append(sentence)
+                    break
         summarize.append(list(summarize_text))
 
     return summarize
